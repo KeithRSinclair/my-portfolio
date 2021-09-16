4d42e5b211b4ade97a86e2667276080abd410319
keithrsinclaire <keithr.sinclair@gmaile.com>
2021-09-16 16:11:45 +0200

    trying to deploy

--- src/components/Project.js
+++ src/components/Project.js
@@ -25,13 +25,13 @@ export default function Project() {
             <section className="grid grid-cols-2 gap-8">
                 {projectData && projectData.map((project, index) => (
                     <article className="relative rounded-lg shadow-xl bg-white p-16">
-                        <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-green-700"><a href={project.link} alt={project.title} target="_blank"  >{project.title}</a></h3>
+                        <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-green-700"><a href={project.link} alt={project.title} target="_blank" rel="noreferrer" >{project.title}</a></h3>
                         <div className="text-gray-500 text-xs space-x-4">
                             <span><strong className="font-bold">Finished on :</strong>{" "}{new Date(project.date).toLocaleDateString()}</span>
                             <span><strong className="font-bold">Company</strong>{" "}{project.place}</span>
                             <span><strong className="font-bold">Type</strong>{" "}{project.projectType}</span>
                             <p className="my-6 text-lg text-gray-700 leading-relaxed">{project.description}</p>
-                            <a href={project.link} alt={project.title} target="_blank" className="text-blue-700 font-bold hover:underline hover:text-green-700">View Project {" "}<span role="img" aria-label="right pointer">➡</span></a>
+                            <a href={project.link} alt={project.title} target="_blank" rel="noreferrer" className="text-blue-700 font-bold hover:underline hover:text-green-700">View Project {" "}<span role="img" aria-label="right pointer">➡</span></a>
 
                         </div>
                     </article>
