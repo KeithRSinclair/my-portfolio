5c28301279cab8102eb7c6c933f2358c14ec57bb
keithrsinclaire <keithr.sinclair@gmaile.com>
2021-09-17 11:28:34 +0200

    Projects page styling

--- src/components/Post.js
+++ src/components/Post.js
@@ -22,10 +22,10 @@ export default function Post() {
                 {postData && postData.map((post, index) => (
                     <article>
                         <Link to={"/post/" + post.slug.current} key={post.slug.current}>
-                            <span className="block h-64 relative rounded shadow leading-snug bg-white" key={index}>
+                            <span className="block h-64 relative rounded shadow leading-snug bg-blue" key={index}>
                                 <img src={post.mainImage.asset.url} alt={post.mainImage.alt} className="w-full h-full rounded object-cover absolute" />
-                                <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
-                                    <h3 className="text-lg font-bold">{post.title}</h3>
+                                <span className="block relative h-full flex justify-center items-end pr-4 pb-4">
+                                    <h3 className="text-lg text-white font-bold bg-blue-900 rounded p-2 justify-center hover:bg-green-900 hover:text-black">{post.title}</h3>
                                 </span>
                             </span>
                         </Link>
--- src/components/Project.js
+++ src/components/Project.js
@@ -22,19 +22,18 @@ export default function Project() {
         <img src={bgImage} alt="circuit board" className="absolute object-cover w-full h-full" style={{ backgroundColor: "#708090", opacity: 0.1, }} />
         <section>
             <h1 className="text-4xl font-bold text-blue-900 font-serif text-center" style={{ textShadow: "#000 2px 2px 3px" }}>My Projects</h1>
-            <section className="grid grid-cols-2 gap-8">
+            <section className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 p-2">
                 {projectData && projectData.map((project, index) => (
                     <article className="relative rounded-lg shadow-xl bg-white p-16">
                         <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-green-700"><a href={project.link} alt={project.title} target="_blank" rel="noreferrer" >{project.title}</a></h3>
                         <div className="text-gray-500 text-xs space-x-4">
-                            <span><strong className="font-bold">Finished on :</strong>{" "}{new Date(project.date).toLocaleDateString()}</span>
-                            <span><strong className="font-bold">Company</strong>{" "}{project.place}</span>
-                            <span><strong className="font-bold">Type</strong>{" "}{project.projectType}</span>
+                            <span><strong className="font-bold">Framework:</strong>{" "}{project.place}</span>
                             <p className="my-6 text-lg text-gray-700 leading-relaxed">{project.description}</p>
-                            <a href={project.link} alt={project.title} target="_blank" rel="noreferrer" className="text-blue-700 font-bold hover:underline hover:text-green-700">View Project {" "}<span role="img" aria-label="right pointer">➡</span></a>
-
+                            <a href={project.link} alt={project.title} target="_blank" rel="noreferrer" className="text-blue-700 font-bold hover:underline hover:text-green-700"><span role="img" aria-label="right pointer">➡</span> {" "}View Project </a>
                         </div>
                     </article>
+
+
                 ))}
             </section>
         </section>
