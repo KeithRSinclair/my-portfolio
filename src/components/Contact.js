0422ab2d0906d3eec85391ec1cf8c47112b20299
keithrsinclaire <keithr.sinclair@gmaile.com>
2021-11-11 10:40:25 +0200

    quick changes to contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -28,52 +28,49 @@ export default function Contact() {
                 <div className="text-lg flex flex-col justify-center">
                     <h1 className="text-4xl mb-6" ><span>Contact Me</span></h1>
                     <form onSubmit={sendEmail} className="w-full max-w-sm">
-                        <div className="md:flex md:items-center mb-6">
+                        <div className="mb-6">
                             <div className="md:w-1/3">
                                 <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-full-name">
                                     Name
                                 </label>
                             </div>
-                            <div className="md:w-2/3">
+                            <div className="md:w-3/3">
                                 <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" name="name" required />
                             </div>
                         </div>
-                        <div className="md:flex md:items-center mb-6">
+                        <div className="mb-6">
                             <div className="md:w-1/3">
                                 <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-email">
                                     E-mail
                                 </label>
                             </div>
-                            <div className="md:w-2/3">
+                            <div className="md:w-3/3">
                                 <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-email" type="email" name="email" required />
                             </div>
                         </div>
-                        <div className="md:flex md:items-center mb-6">
+                        <div className="mb-6">
                             <div className="md:w-1/3">
                                 <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-subject">
                                     Subject
                                 </label>
                             </div>
-                            <div className="md:w-2/3">
+                            <div className="md:w-3/3">
                                 <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-subject" type="text" name="subject" required />
                             </div>
                         </div>
-                        <div className="md:flex md:items-center mb-6">
+                        <div className="mb-6">
                             <div className="md:w-1/3">
                                 <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-message">
                                     Message
                                 </label>
                             </div>
-                            <div className="md:w-2/3">
+                            <div className="md:w-3/3">
                                 <textarea className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" rows="8" id="inline-message" type="text" name="message" required />
                             </div>
                         </div>
 
                         <div className="md:flex md:items-center">
-                            <div className="md:w-1/3"></div>
-                            <div className="md:w-2/3">
-                                <button className="bg-blue-600 hover:bg-blue-700 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center" type="submit"><span>Send</span><PaperAirplaneIcon className="h-6 transform rotate-90" /></button>
-                            </div>
+                            <button className="bg-blue-600 hover:bg-blue-700 text-gray-800 font-bold py-2 px-8 rounded inline-flex items-center" type="submit"><span>Send</span><PaperAirplaneIcon className="h-6 transform rotate-90" /></button>
                         </div>
                     </form>
                 </div>
