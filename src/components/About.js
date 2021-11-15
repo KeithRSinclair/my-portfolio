b36622e400d4ffd4df85a1de7d04898b658d4f35
keithrsinclaire <keithr.sinclair@gmaile.com>
2021-11-15 10:30:55 +0200

    adjust padding

--- src/components/About.js
+++ src/components/About.js
@@ -34,7 +34,7 @@ export default function About() {
             <section className="bg-blue-800 rounded-lg shadow-2xl lg:flex p-20" >
                 <img src={urlFor(author.authorImage).url()} className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8" alt={author.name} />
                 <div className="text-lg flex flex-col justify-center">
-                    <h2 className="text-6xl text-white mb-4 p-6" ><span>{author.name}</span></h2>
+                    <h2 className="text-6xl text-white mb-4 p-3" ><span>{author.name}</span></h2>
                     <div>
                         <BlockContent blocks={author.bio} projectId="wm77me1p" dataset="production" />
                     </div>
