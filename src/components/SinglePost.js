15c030d0b4500aac42c9ddc7fe5af64878f847c9
keithrsinclaire <keithr.sinclair@gmaile.com>
2021-11-16 11:08:08 +0200

    author image bg

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -39,13 +39,13 @@ export default function SinglePost() {
 
 
     return (
-        <main className="bg-gray-200 min-h-screen p-12">
+        <main className="p-5">
             <article className="container shadow-lg mx-auto bg-blue-100 rounded-lg" >
                 <header className="relative">
                     <div className="absolute h-full w-full flex items-center justify-center p-8" >
-                        <div className="bg-white rounded p-12">
-                            <h1 className="text-3xl lg:text-6xl mb-4">{singlePost.title}</h1>
-                            <div className="flex justify-center text-gray-800" ><img src={urlFor(singlePost.authorImage).url()} alt={singlePost.name} className="w-10 h-10 rounded-full" /><p className="flex text-center pl-2" >written by{" "}{singlePost.name}</p></div>
+                        <div className="bg-black bg-opacity-75 rounded-lg p-12">
+                            <h1 className="text-3xl lg:text-6xl mb-4 text-white">{singlePost.title}</h1>
+                            <div className="flex justify-center text-gray-800" ><img src={urlFor(singlePost.authorImage).url()} alt={singlePost.name} className="w-10 h-10 rounded-full bg-blue-800" /><p className="flex text-center pl-2 text-white" >written by{" "}{singlePost.name}</p></div>
                         </div>
                     </div>
                     <img src={singlePost.mainImage.asset.url} alt={singlePost.title} className="w-full object-cover rounded" style={{ height: "600px" }} />
